32f04e2db404bce58a16b6dc1229e3d72fddfd4c
HighwayRob <robsobol@yahoo.com>
2022-04-04 17:11:35 -0400

    xxx

--- src/main/resources/DataSource.java
+++ src/main/resources/DataSource.java
@@ -7,7 +7,7 @@ public class DataSource {
     public DriverManagerDataSource dataSource(){
         DriverManagerDataSource source = new DriverManagerDataSource();
         source.setDriverClassName("org.postgresql.Driver");
-        source.setUrl("jdbc:postgres://xmmazemfflgbkj:a323bc337c559fffd0ec7029daaf60f03712cdb1df41ca8629fe8429f0a7a607@ec2-54-85-113-73.compute-1.amazonaws.com:5432/d423nfcf5dbjf7");
+        source.setUrl("jdbc:postgres://ec2-54-85-113-73.compute-1.amazonaws.com:5432/d423nfcf5dbjf7");
         source.setUsername("xmmazemfflgbkj");
         source.setPassword("a323bc337c559fffd0ec7029daaf60f03712cdb1df41ca8629fe8429f0a7a607");
         return source;
